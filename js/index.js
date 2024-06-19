126451aced5b993fa4d0abbb64721dc5b6264289
Andy <obrezkov.andy@gmail.com>
2024-06-19 21:48:03 +0300

    Add js code

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('keydown', playSound);
+document.addEventListener('keyup', returnOriginalStyle);
+
+function playSound(event) {
+  const audioNode = document.querySelector(`audio[data-key="${event.code}"]`);
+  if (!audioNode) return;
+  audioNode.currentTime = 0;
+  audioNode.play();
+  const itemNode = document.querySelector(`.item[data-key="${event.code}"]`);
+  itemNode.classList.add('item_active');
+}
+
+function returnOriginalStyle(event) {
+  const itemNode = document.querySelector(`.item[data-key="${event.code}"]`);
+  itemNode.classList.remove('item_active');
+}
